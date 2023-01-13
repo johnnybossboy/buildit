38c9c41a86934d4ea27301d5aae6faadc5846491
John Bosschert (Win Laptop) <john.bosschert@gmail.com>
2023-01-13 14:07:44 +0100

    final

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Recipe;
-use Mockery\Undefined;
 
 class RecipesController extends Controller
 {
@@ -11,7 +10,7 @@ class RecipesController extends Controller
     {
         // $recipes = Recipe::Paginate(10); // Pagination works, but not sorting.
         // $recipes = Recipe::all()->sortDesc(); // this works, but no pagination!
-        $recipes = Recipe::orderBy('created_at', 'desc')->simplePaginate(10);
+        $recipes = Recipe::orderBy('created_at', 'desc')->simplePaginate(10); // Both work!
 
         return view('recipeshome', ['recipes' => $recipes]);
     }
@@ -66,39 +65,5 @@ class RecipesController extends Controller
 
         return redirect('/');
     }
-
-// public function saveItem(Request $request)
-// {
-//     \Log::info(json_encode($request->all()));
-
-//     // Below uses the modal ListItem that is created via a migration command.
-//     $newListItem = new listItem;
-//     $newListItem->name = $request->listItem;
-//     $newListItem->is_complete = 0;
-
-//     // Save in the database.
-//     $newListItem->save();
-
-//     // Delete from database.
-//     // $newListItem->delete();
-
-//     // You are at /saveItem.
-//     // Ensure the user is redirected to the home page to prevent re-submitting form data upon refreshing the page.
-//     return redirect('/');
-// }
-
-// public function markComplete($id)
-// {
-//     \Log::info($id);
-//     $listItem = ListItem::find($id);
-//     if ($listItem->is_complete === 0) {
-//         $listItem->is_complete = 1;
-//     } else {
-//         $listItem->is_complete = 0;
-//     }
-
-//     $listItem->save();
-//     return redirect('/');
-// }
 }
 ?>
